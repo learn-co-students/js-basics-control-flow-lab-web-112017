776ed2b8cdd1c8fa735c546f03d465ea13718d86
yamunanavada <yamunanavada@gmail.com>
2018-01-07 16:01:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(num) {
+  if(num <= 400){
+    return "This one is on me!"
+  } else if (num > 2000 && num <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (num > 2500) {
+    return 'No can do.'
+  }
+};
+
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
+}
+
+function switchOnCharmFromTip(gen){
+
+  switch (gen) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+
+}
