2bd4a08bbc59e785599bdba2e3a6ea887d30f06b
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-07 11:41:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let price = 0;
+  if (feet <= 400) {
+    price = 0
+    return "This one is on me!"
+  } else if (feet >= 2000 && feet <= 2500) {
+    let price = 30;
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  // let isNewYork = 'NYC';
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+}
+
+function switchOnCharmFromTip(tip) {
+  //let response;
+
+  switch(tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+  }
+
+}
